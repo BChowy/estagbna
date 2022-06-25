d05b164c545b86be9fe61a7247e4a7fad326649a
Bushra <bushrashodari@gmail.com>
2022-06-25 18:04:29 +0300

    Add 'theme' and 'home'

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-import './home'
+import 'package:flutter/services.dart';
+
+import 'homepage/home.dart';
+import './app_themes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    //only portrait orientation is allowed
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home()
+      debugShowCheckedModeBanner: false,
+      title: 'فاستجبنا له',
+      //App theme
+      theme: AppTheme.darkTheme,
+      home: const Home(),
     );
   }
 }
