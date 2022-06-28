4f86af690c0aaa3bb4b8f3e78c7bd7e30f53dca8
Bushra <bushrashodari@gmail.com>
2022-06-28 07:30:47 +0300

    - Delete 'text direction - rtl' (already added it to home.dart) - Add 'Stack widget' to make divider line behind headline.

--- lib/homepage/heading.dart
+++ lib/homepage/heading.dart
@@ -13,24 +13,36 @@ class _HeadingState extends State<Heading> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return (Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text('فاسْتَجَبْنَا لَهُ',
-            textDirection: TextDirection.rtl,
-            overflow: TextOverflow.ellipsis, //what?
-            style: Theme.of(context).textTheme.headline1),
+        SizedBox(
+          height: SizeConfig.blockSizeVertical * 2,
+        ),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+                child: Divider(
+              thickness: 10,
+              indent: SizeConfig.blockSizeHorizontal * 20,
+              endIndent: SizeConfig.blockSizeHorizontal * 20,
+              color: Theme.of(context).cardColor,
+            )),
+            Text('فاسْتَجَبْنَا لَهُ',
+                overflow: TextOverflow.ellipsis, //what?
+                style: Theme.of(context).textTheme.headline1),
+          ],
+        ),
         SizedBox(
           height: SizeConfig.blockSizeVertical * 3,
         ),
         Text('ادْعُونِي أَسْتَجِبْ لَكُمْ - غافر',
-            textDirection: TextDirection.rtl,
             overflow: TextOverflow.ellipsis, //what?
             style: Theme.of(context).textTheme.headline5),
         SizedBox(
           height: SizeConfig.blockSizeVertical * 2,
         ),
         Text('وَاذْكُرْ رَبَّكَ إِذَا نَسِيتَ - الكهف',
-            textDirection: TextDirection.rtl,
             overflow: TextOverflow.ellipsis, //what?
             style: Theme.of(context).textTheme.headline5),
         SizedBox(
