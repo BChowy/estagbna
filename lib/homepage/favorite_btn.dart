5f421683b7762dd1ed18b72a5586def4e5d9d7dd
Bushra <bushrashodari@gmail.com>
2022-06-28 07:40:23 +0300

    - change widgets order

--- lib/homepage/favorite_btn.dart
+++ lib/homepage/favorite_btn.dart
@@ -2,32 +2,32 @@ import 'package:flutter/material.dart';
 import '../size_config.dart';
 
 class FavoriteBtn extends StatelessWidget {
-  const FavoriteBtn({ Key? key }) : super(key: key);
+  const FavoriteBtn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-          //width: double.infinity,
-          height: SizeConfig.blockSizeVertical * 6,
-          child: ElevatedButton(
-              onPressed: () {},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                    size: 16,
-                  ),
-                  SizedBox(
-                    width: SizeConfig.blockSizeHorizontal * 2,
-                  ),
-                  Text(
-                    'المفضلة',
-                    style: Theme.of(context).textTheme.button,
-                  ),
-                ],
-              )),
-        );
+      //width: double.infinity,
+      height: SizeConfig.blockSizeVertical * 6,
+      child: ElevatedButton(
+          onPressed: () {},
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'المفضلة',
+                style: Theme.of(context).textTheme.button,
+              ),
+              SizedBox(
+                width: SizeConfig.blockSizeHorizontal * 2,
+              ),
+              const Icon(
+                Icons.favorite,
+                color: Colors.red,
+                size: 16,
+              ),
+            ],
+          )),
+    );
   }
 }
