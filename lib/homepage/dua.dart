2d96d3cc0244e890f8b083da0bce59334ffe6d27
Bushra <bushrashodari@gmail.com>
2022-06-28 07:27:55 +0300

    - Change 'getData()' method. - Add 'GridView' of 'FutureBuilder' to show data.

--- lib/homepage/dua.dart
+++ lib/homepage/dua.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../size_config.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart';
+import '../data_model.dart';
+import '../info_API.dart';
 
 class Dua extends StatefulWidget {
   const Dua({Key? key}) : super(key: key);
@@ -11,82 +11,96 @@ class Dua extends StatefulWidget {
 }
 
 class _DuaState extends State<Dua> {
-  List _categories = [];
-  List _desc = [];
-  int i = 0;
-
-  Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/duaData.json');
-    final data = await json.decode(response);
-    setState(() {
-      //for (var category in data["dua"]![i].keys) {
-      _categories = data['dua'];
-      print(i);
-      //  i += 1;
-      //}
-    });
-  }
-
-  @override
-  void initState() {
-    print('object');readJson();
-    super.initState();
-    
-    
+  List<String> getData(DataModel? data) {
+    List<String> cateNameList = [];
+    //List<Object> cateIdList = [];
+    int length = data?.category.length ?? 0;
+    for (var i = 0; i < length; i++) {
+      final detail = data?.category.elementAt(i);
+      cateNameList.add(detail?.name ?? '');
+      //cateIdList.add(detail?.cateid ?? '');
+      //print(detail?.cateid);
+    }
+    return cateNameList;
   }
 
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Column(
-      children: [
-        Padding(
-          padding:
-              EdgeInsets.symmetric(vertical: SizeConfig.blockSizeVertical * 2),
-          child: (Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Row(children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 2),
-                    child: Icon(
-                      Icons.keyboard_arrow_left_sharp,
-                      size: 20,
-                    ),
-                  ),
-                  Text('إظهار الكل', style: Theme.of(context).textTheme.caption)
-                ]),
-              ),
-              Text(
-                'الأدعية',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ],
-          )),
-        ),
-        /* Container(
-          color: Colors.amber,
-          height: 30,
-          child: ListView(),
-        ) */
-        _categories.isNotEmpty
-            ? Expanded(
-                child: ListView.builder(
-                  itemCount: _categories.length,
-                  itemBuilder: (context, index) {
-                    return Container(width: SizeConfig.blockSizeHorizontal*5,
-                      margin: const EdgeInsets.all(10),
-                      child: Text(_categories[index]),
-                    );
-                  },
+    return Expanded(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+                vertical: SizeConfig.blockSizeVertical * 2),
+            child: (Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'الأدعية',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-              )
-            : Container(
-                child: Text('nothing to show'),
-              ),
-      ],
+                InkWell(
+                  onTap: () {},
+                  child: Row(children: [
+                    Text('إظهار الكل',
+                        style: Theme.of(context).textTheme.caption),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 2),
+                      child: Icon(
+                        Icons.keyboard_arrow_left_sharp,
+                        size: 20,
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
+            )),
+          ),
+          FutureBuilder<DataModel>(
+              future: InfoAPI('assets/duaData.json').getDataLocally(context),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+                final List<String> list = getData(data);
+
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  height: SizeConfig.blockSizeVertical * 20,
+                  // width: double.infinity,
+
+                  child: GridView.count(
+                      crossAxisCount: 1,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8.0, horizontal: 10.0),
+                      scrollDirection: Axis.horizontal,
+                      childAspectRatio: 1,
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      children: List.generate(
+                        list.length,
+                        (index) {
+                          return Card(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16)),
+                              //clipBehavior: Clip.antiAlias,
+                              color: Theme.of(context).cardColor,
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Center(
+                                    child: Text(
+                                  list[index],
+                                  style: Theme.of(context).textTheme.bodyText1,
+                                )),
+                              ));
+                        },
+                      )),
+                );
+              })
+        ],
+      ),
     );
   }
 }
