d03fa2a088d826cf64935d68a972bf786c9de2f7
Bushra <bushrashodari@gmail.com>
2022-06-29 12:22:53 +0300

    Add images, changed 'divider' width to depend on text width.

--- lib/homepage/lists.dart
+++ lib/homepage/lists.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../size_config.dart';
 import '../data_model.dart';
 import '../info_API.dart';
+import '../other_pages/category_page.dart';
 
 class Dua extends StatefulWidget {
   const Dua({Key? key}) : super(key: key);
@@ -11,6 +12,10 @@ class Dua extends StatefulWidget {
 }
 
 class _DuaState extends State<Dua> {
+  final duaFilePath = 'assets/duaData.json';
+  final athkarFilePath = 'assets/athkarData.json';
+  final listBGimagePath = 'assets/images/motif-1.png';
+
   List<String> getData(DataModel? data) {
     List<String> cateNameList = [];
     //List<Object> cateIdList = [];
@@ -28,143 +33,174 @@ class _DuaState extends State<Dua> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Expanded(
-      child: ListView(padding: EdgeInsets.zero,
+      child: ListView(
+        padding: EdgeInsets.zero,
         children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: SizeConfig.blockSizeVertical * 2),
-            child: (Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'الأدعية',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Row(children: [
-                    Text('إظهار الكل',
-                        style: Theme.of(context).textTheme.caption),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 2),
-                      child: Icon(
-                        Icons.keyboard_arrow_left_sharp,
-                        size: 20,
-                      ),
-                    ),
-                  ]),
-                ),
-              ],
-            )),
-          ),
           FutureBuilder<DataModel>(
-              future: InfoAPI('assets/duaData.json').getDataLocally(context),
+              future: InfoAPI(duaFilePath).getDataLocally(context),
               builder: (context, snapshot) {
                 final data = snapshot.data;
                 final List<String> list = getData(data);
 
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  height: SizeConfig.blockSizeVertical * 20,
-                  // width: double.infinity,
-
-                  child: GridView.count(
-                      crossAxisCount: 1,
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 10.0),
-                      scrollDirection: Axis.horizontal,
-                      childAspectRatio: 1,
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      children: List.generate(
-                        list.length,
-                        (index) {
-                          return Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16)),
-                              //clipBehavior: Clip.antiAlias,
-                              color: Theme.of(context).cardColor,
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Center(
-                                    child: Text(
-                                  list[index],
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                )),
-                              ));
-                        },
+                return Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: SizeConfig.blockSizeVertical * 2),
+                      child: (Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'الأدعية',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Row(children: [
+                              Text('إظهار الكل',
+                                  style: Theme.of(context).textTheme.caption),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 2),
+                                child: Icon(
+                                  Icons.keyboard_arrow_left_sharp,
+                                  size: 20,
+                                ),
+                              ),
+                            ]),
+                          ),
+                        ],
                       )),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Theme.of(context).primaryColor,
+                          image: DecorationImage(
+                              colorFilter: const ColorFilter.mode(
+                                  Colors.black26, BlendMode.dstIn),
+                              image: AssetImage(listBGimagePath),
+                              fit: BoxFit.cover)),
+                      height: SizeConfig.blockSizeVertical * 20,
+                      // width: double.infinity,
+
+                      child: GridView.count(
+                          crossAxisCount: 1,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 10.0),
+                          scrollDirection: Axis.horizontal,
+                          childAspectRatio: 1,
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: List.generate(
+                            list.length,
+                            (index) {
+                              return Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)),
+                                  //clipBehavior: Clip.antiAlias,
+                                  color: Theme.of(context).cardColor,
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Center(
+                                        child: Text(
+                                      list[index],
+                                      style:
+                                          Theme.of(context).textTheme.bodyText1,
+                                    )),
+                                  ));
+                            },
+                          )),
+                    ),
+                  ],
                 );
-              }), Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: SizeConfig.blockSizeVertical * 2),
-            child: (Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'الأذكار',
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Row(children: [
-                    Text('إظهار الكل',
-                        style: Theme.of(context).textTheme.caption),
-                    const Padding(
-                      padding: EdgeInsets.only(top: 2),
-                      child: Icon(
-                        Icons.keyboard_arrow_left_sharp,
-                        size: 20,
-                      ),
-                    ),
-                  ]),
-                ),
-              ],
-            )),
-          ),
+              }),
           FutureBuilder<DataModel>(
-              future: InfoAPI('assets/athkarData.json').getDataLocally(context),
+              future: InfoAPI(athkarFilePath).getDataLocally(context),
               builder: (context, snapshot) {
                 final data = snapshot.data;
                 final List<String> list = getData(data);
 
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  height: SizeConfig.blockSizeVertical * 20,
-                  // width: double.infinity,
-
-                  child: GridView.count(
-                      crossAxisCount: 1,
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 10.0),
-                      scrollDirection: Axis.horizontal,
-                      childAspectRatio: 1,
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      children: List.generate(
-                        list.length,
-                        (index) {
-                          return Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16)),
-                              //clipBehavior: Clip.antiAlias,
-                              color: Theme.of(context).cardColor,
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Center(
-                                    child: Text(
-                                  list[index],
-                                  style: Theme.of(context).textTheme.bodyText1,
-                                )),
-                              ));
-                        },
+                return Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          vertical: SizeConfig.blockSizeVertical * 2),
+                      child: (Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'الأذكار',
+                            style: Theme.of(context).textTheme.headline6,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Directionality(
+                                        textDirection: TextDirection.rtl,
+                                        child: CategoryPage(
+                                          fileName: athkarFilePath,
+                                          names: list,
+                                        ))),
+                              );
+                            },
+                            child: Row(children: [
+                              Text('إظهار الكل',
+                                  style: Theme.of(context).textTheme.caption),
+                              const Padding(
+                                padding: EdgeInsets.only(top: 2),
+                                child: Icon(
+                                  Icons.keyboard_arrow_left_sharp,
+                                  size: 20,
+                                ),
+                              ),
+                            ]),
+                          ),
+                        ],
                       )),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Theme.of(context).primaryColor,
+                          image: DecorationImage(
+                              colorFilter: const ColorFilter.mode(
+                                  Colors.black26, BlendMode.dstIn),
+                              image: AssetImage(listBGimagePath),
+                              fit: BoxFit.cover)),
+                      height: SizeConfig.blockSizeVertical * 20,
+                      // width: double.infinity,
+
+                      child: GridView.count(
+                          crossAxisCount: 1,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 10.0),
+                          scrollDirection: Axis.horizontal,
+                          childAspectRatio: 1,
+                          physics: const AlwaysScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          children: List.generate(
+                            list.length,
+                            (index) {
+                              return Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(16)),
+                                  //clipBehavior: Clip.antiAlias,
+                                  color: Theme.of(context).cardColor,
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: Center(
+                                        child: Text(
+                                      list[index],
+                                      style:
+                                          Theme.of(context).textTheme.bodyText1,
+                                    )),
+                                  ));
+                            },
+                          )),
+                    ),
+                  ],
                 );
               })
         ],
