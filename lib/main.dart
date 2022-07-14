7efc2401bf0b7badde113bc7b4afd52d05736070
Bushra <bushrashodari@gmail.com>
2022-07-14 11:06:54 +0300

    - add 'changeTheme' to change app mode. - add 'saveTheme' save user preffered mode. - add 'getTheme' to get saved theme for user. - Change 'theme' depend on 'dark' variable.

--- lib/main.dart
+++ lib/main.dart
@@ -3,37 +3,74 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'homepage/home.dart';
-import './app_themes.dart';
+import 'theme/app_themes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+}
+
+class _MyAppState extends State<MyApp> {
+  bool dark = true;
+
+  void changeTheme(bool isDark) {
+    setState(() {
+      dark = isDark;
+    });
+  }
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  late Future _darkF;
+
+  saveTheme() async {
+    SharedPreferences pref = await _prefs;
+    pref.setBool('theme', dark);
+  }
+
+  _getTheme() async {
+    _darkF = _prefs.then((SharedPreferences prefs) {
+      return prefs.getBool('theme') ?? true;
+      
+    });
+    dark = await _darkF;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getTheme();
+  }
 
   @override
   Widget build(BuildContext context) {
     //only portrait orientation is allowed
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('ar', ''), // Arabic, no country code
-      ],
-      locale: const Locale("ar", ''),
-      title: 'فاستجبنا له',
-      //App theme
-
-      theme: AppTheme.darkTheme,
-      home: const Home(),
-    );
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('ar', ''), // Arabic, no country code
+        ],
+        locale: const Locale("ar", ''),
+        title: 'فاستجبنا له',
+        theme: dark ? AppTheme.darkTheme : AppTheme.lightTheme,
+        //darkTheme: AppTheme.darkTheme,
+        home: const Home(),
+      );
+    }
   }
-}
